51982110e0ebfb80a25a3ea27fb5b126d6244b3c
José Campelo <josecampelojr@gmail.com>
2024-12-18 20:13:13 -0300

    Criação do projeto

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,13 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        // Objetivo da aula: Criar um ninja -> Naruto
+
+        int idade = 16; // Valor máximo: 2147483647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldobancario = 777L; // Valor máximo: 9223372036854775807
+    }
+}
